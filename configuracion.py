aa3be1ecfe916cdef0e2e15c883bc0c56bc5acaa
d-alvarado-iberoleon <27382@iberoleon.edu.mx>
2024-11-21 17:52:56 -0600

    Cambiar el número de atracciones a 10 en main

--- configuracion.py
+++ configuracion.py
@@ -1,2 +1,4 @@
 configuracion = {'atracciones': 10, 'restaurantes': 5}
 configuracion['atracciones'] = 20 
+"configuracion = {'atracciones': 10, 'restaurantes': 5}" 
+"configuracion['atracciones'] = 10" 
