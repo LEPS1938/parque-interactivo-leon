641d7ebe18504b64205cbc1e5eae62d096b963e3
d-alvarado-iberoleon <27382@iberoleon.edu.mx>
2024-11-28 05:24:22 -0600

    fix bug in configuracio.py

--- configuracion.py
+++ configuracion.py
@@ -2,3 +2,4 @@ configuracion = {'atracciones': 10, 'restaurantes': 5}
 configuracion['atracciones'] = 20 
 "configuracion = {'atracciones': 10, 'restaurantes': 5}" 
 "configuracion['atracciones'] = 10" 
+configuracion['atracciones'] = 10
