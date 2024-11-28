4afdcb0bab9ed7ce61709e79fda053d14088806b
Luis Antonio López Abaunza <194427-3@iberoleon.edu.mx>
2024-11-28 12:08:15 -0600

    E4: Fin del examen

--- atracciones.py
+++ atracciones.py
@@ -6,3 +6,4 @@ atracciones = [
 ]
 atracciones_antiguas = ['Rueda de la Fortuna', 'Caballitos']
 atracciones_modernas = ['Simulador Virtual', 'Carrusel Moderno']
+atracciones = ['Carros chocones', 'Barco pirata', 'Sillas voladoras']
